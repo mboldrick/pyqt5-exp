5829352b0dab7665e320665a178d4a0fa2203bba
Mike Boldrick <mb@boldrick.net>
2023-11-28 09:54:10 -0800

    MainWindow subclasses QMainWindow

--- qt_template.py
+++ qt_template.py
@@ -4,7 +4,7 @@ from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
 
 
-class MainWindow(qtw.QWidget):
+class MainWindow(qtw.QMainWindow):
     def __init__(self):
         """MainWindow constructor"""
         super().__init__()
