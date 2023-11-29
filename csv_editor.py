a054caf3aada868c2c180ccbdd66a5f54b4a7969
Mike Boldrick <mb@boldrick.net>
2023-11-28 18:10:58 -0800

    create data model for csv_editor.py

--- csv_editor.py
+++ csv_editor.py
@@ -3,6 +3,7 @@ import csv
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
+from PyQt5.QtCore import QModelIndex
 
 
 class CsvTableModel(qtc.QAbstractTableModel):
@@ -15,6 +16,61 @@ class CsvTableModel(qtc.QAbstractTableModel):
             csvreader = csv.reader(fh)
             self._headers = next(csvreader)
             self._data = list(csvreader)
+
+    def rowCount(self, parent):
+        return len(self._data)
+
+    def columnCount(self, parent):
+        return len(self._headers)
+
+    def data(self, index, role):
+        if role in (qtc.Qt.DisplayRole, qtc.Qt.EditRole):
+            return self._data[index.row()][index.column()]
+
+    def headerData(self, section, orientation, role):
+        if orientation == qtc.Qt.Horizontal and role == qtc.Qt.DisplayRole:
+            return self._headers[section]
+        else:
+            return super().headerData(section, orientation, role)
+
+    def sort(self, column, order):
+        self.layoutAboutToBeChanged.emit()  # needs to be emitted before a sort
+        self._data.sort(key=lambda x: x[column])
+        if order == qtc.Qt.DescendingOrder:
+            self._data.reverse()
+        self.layoutChanged.emit()  # needs to be emitted after a sort
+
+    def flags(self, index):
+        return super().flags(index) | qtc.Qt.ItemIsEditable
+
+    def setData(self, index, value, role):
+        if index.isValid() and role == qtc.Qt.EditRole:
+            self._data[index.row()][index.column()] = value
+            self.dataChanged.emit(index, index, [role])
+            return True
+        else:
+            return False
+
+    def insertRows(self, position, rows, parent):
+        self.beginInsertRows(
+            parent or qtc.QModelndex(),
+            position,
+            position + rows - 1,
+        )
+        for i in range(rows):
+            default_row = [""] * len(self._headers)
+            self._data.insert(position, default_row)
+        self.endInsertRows()
+
+    def removeRows(self, position, rows, parent):
+        self.beginRemoveRows(
+            parent or qtc.QModelIndex(),
+            position,
+            position + rows - 1,
+        )
+        for i in range(rows):
+            del self._data[position]
+        self.endRemoveRows()
 
 
 class MainWindow(qtw.QMainWindow):
