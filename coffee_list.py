526411f2fa750442a4e72b1bb003129d83bf0991
Mike Boldrick <mb@boldrick.net>
2023-11-29 19:55:16 -0800

    connect to db in coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -6,9 +6,12 @@ from PyQt5 import QtSql as qts
 
 
 class CoffeeForm(qtw.QWidget):
+    """Form to display/edit all info about a coffee"""
+
     def __init__(self, roasts):
         super().__init__()
         self.setLayout(qtw.QFormLayout())
+
         self.coffee_brand = qtw.QLineEdit()
         self.layout().addRow("Brand:", self.coffee_brand)
         self.coffee_name = qtw.QLineEdit()
@@ -37,6 +40,36 @@ class MainWindow(qtw.QMainWindow):
         """MainWindow constructor"""
         super().__init__()
         # Main UI code goes here
+        self.stack = qtw.QStackedWidget()
+        self.setCentralWidget(self.stack)
+
+        # Connect to the database
+        self.db = qts.QSqlDatabase.addDatabase("QSQLITE")
+        self.db.setDatabaseName("coffee.db")
+        if not self.db.open():
+            error = self.db.lastError().text()
+            qtw.QMessageBox.critical(
+                None, "DB Connection Error", f"Could not open database file: {error}"
+            )
+            sys.exit(1)
+
+        # Check for missing tables
+        required_tables = {"roasts", "coffees", "reviews"}
+        tables = self.db.tables()
+        missing_tables = required_tables - set(tables)
+        if missing_tables:
+            qtw.QMessageBox.critical(
+                None,
+                "DB Integrity Error",
+                f"Missing tables: {missing_tables}. Please repair the database.",
+            )
+            sys.exit(1)
+
+        # Make a query
+        query = self.db.exec("SELECT count(*) FROM coffees")
+        query.next()
+        count = query.value(0)
+        print(f"There are {count} coffees in the databae.")
 
         # End main UI code
         self.show()
